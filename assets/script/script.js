579e33348428475c7ea9dc2ba719a68b12ba1d7b
Preston Foote <prestonwfoote@gmail.com>
2021-10-27 20:40:49 -0400

    created html layout and JS question structure

---
+++ assets/script/script.js
@@ -0,0 +1,31 @@
+//JS Quiz
+
+var questions = [
+    {
+        title: "question 1",
+        alternatives:["anwser 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 1"
+    },
+    {
+        title: "question 2",
+        alternatives:["anwser 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 1"
+    },
+    {
+        title: "question 3",
+        alternatives:["anwser 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 1"
+    },
+    {
+        title: "question 4",
+        alternatives:["anwser 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 1"
+    },
+    {
+        title: "question 5",
+        alternatives:["anwser 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 1"
+    },
+
+];
+
