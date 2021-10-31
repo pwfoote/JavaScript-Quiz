4ca92a67bd4cecfeb536722c3c1ec92caa0478c5
prestonwfoote <prestonwfoote@gmail.com>
2021-10-31 12:18:02 -0400

    re structured html, css and JS files.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,31 +1,109 @@
 //JS Quiz
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-var questions = [
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    console.log('started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer (e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    Array.from(answerButtonsElement.children).forEach(button => {
+        clearStatusClass(button)
+    })
+    setStatusClass(selectedButton, correct)
+    if(shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    }else {
+        startButton.innerText = 
+    
+    nextButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    //clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+        
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
     {
-        title: "question 1",
-        alternatives:["anwser 1", "answer 2", "answer 3", "answer 4"],
-        answer: "answer 1"
-    },
-    {
-        title: "question 2",
-        alternatives:["anwser 1", "answer 2", "answer 3", "answer 4"],
-        answer: "answer 1"
-    },
-    {
-        title: "question 3",
-        alternatives:["anwser 1", "answer 2", "answer 3", "answer 4"],
-        answer: "answer 1"
-    },
-    {
-        title: "question 4",
-        alternatives:["anwser 1", "answer 2", "answer 3", "answer 4"],
-        answer: "answer 1"
-    },
-    {
-        title: "question 5",
-        alternatives:["anwser 1", "answer 2", "answer 3", "answer 4"],
-        answer: "answer 1"
+        question: 'choose answer 1',
+        answers: [
+            {text: '1', correct: true},
+            {text: '2', correct: false},
+            {text: '3', correct:false},
+            {text: '4', correct: false}
+
+        ]
     },
 
-];
+    {
+        question: 'choose answer 2',
+        answers: [
+            {text: '1', correct: false},
+            {text: '2', correct: true},
+            {text: '3', correct:false},
+            {text: '4', correct: false}
+
+        ]
+    },
+]
+
+
+//create timer and deduction points
+//end of quiz eneter high score
 
